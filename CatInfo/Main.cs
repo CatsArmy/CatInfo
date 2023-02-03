b7042c83a69365d64726ebec7347d8be0402408d
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-02-03 12:27:32 +0200

    Menacing beep

--- CatInfo/Main.cs
+++ CatInfo/Main.cs
@@ -531,7 +531,7 @@ namespace CatInfo
         }
         private static void NewGUI(GameObject menu)
         {
-            MenuHandler.CreateText(" Toggle" + ModName + "Stats", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText(" Toggle" + ModName + "Stats", menu, out TextMeshProUGUI _, 70);
             MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             GameObject BasicStats = MenuHandler.CreateMenu("Basic Stats Extended", () => { }, menu, 60, true, true, menu.transform.parent.gameObject);
             BasicStatsToggles(BasicStats);
@@ -543,47 +543,71 @@ namespace CatInfo
             AdvancedGunStatsToggles(AdvancedGunStats);
             GameObject ChargedStats = MenuHandler.CreateMenu("Plasma Charged Stats", () => { }, menu, 60, true, true, menu.transform.parent.gameObject);
             ChargedStatsToggles(ChargedStats);
+            
+            GameObject PresetOn = MenuHandler.CreateButton("",menu,60,PresetAllOn(),true,menu);
+        }
+        private static void PresetAllOn(GameObject menu)
+        {
+
         }
         private static void BasicStatsToggles(GameObject menu)
         {
-            MenuHandler.CreateText("This Cannot Toggle The Defualt Basic Stats That TabInfo Adds Only The Ones That CatInfo Adds", menu, out TextMeshProUGUI _, 55);
+            MenuHandler.CreateText("This Cannot Toggle The Defualt Basic Stats That TabInfo Adds Only The Ones That CatInfo Adds", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(EchoBlocks, "Echo Blocks", menu, value => EchoBlocks = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(AttackSpeed, "Attack Speed", menu, value => AttackSpeed = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(LifeSteal, "Life Steal", menu, value => LifeSteal = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletBounces, "Bullet Bounces", menu, value => BulletBounces = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Bursts, "Bursts", menu, value => Bursts = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Bullets, "Bullets", menu, value => Bullets = value, fontSize: 50, forceUpper: false);
         }
         private static void UsefulStatsToggles(GameObject menu)
         {
-            MenuHandler.CreateText("Unblockable Bullets is just unblockable bullets", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText("Unblockable Bullets is just unblockable bullets", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(UnblockableBullets, "Unblockable Bullets", menu, value => UnblockableBullets = value, fontSize: 50, forceUpper: false);
-            MenuHandler.CreateText("Ghost Bullets lets your bullets pass through walls and shit", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
+            MenuHandler.CreateText("Ghost Bullets lets your bullets pass through walls and shit", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(GhostBullet, "Ghost Bullets", menu, value => GhostBullet = value, fontSize: 50, forceUpper: false);
-            MenuHandler.CreateText("Plasma Gun aka Use Charge like plasma rifle (WWC) or plasma shotgun (WWC)", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
+            MenuHandler.CreateText("Plasma Gun aka Use Charge like plasma rifle (WWC) or plasma shotgun (WWC)", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Causes the gun to use a charge system for firing. None of the charge stats work without this being set to true.", menu, out TextMeshProUGUI _, 40);
             MenuHandler.CreateToggle(UseCharge, "Chargable Plasma Gun", menu, value => UseCharge = value, fontSize: 50, forceUpper: false);
-            MenuHandler.CreateText("No Full Auto In The Building lol u cant hold down the mouse like demonic pact", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
+            MenuHandler.CreateText("No Full Auto In The Building lol u cant hold down the mouse like demonic pact", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(NoFullAutoInTheBuilding, "Not An Automatic Gun", menu, value => NoFullAutoInTheBuilding = value, fontSize: 50, forceUpper: false);
-            MenuHandler.CreateText("%Ammo Regen is the percentage of ammo regenerated each second", menu, out TextMeshProUGUI _, 60);
+            MenuHandler.CreateText("%Ammo Regen is the percentage of ammo regenerated each second", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(AmmoRegen, "%Ammo Regen", menu, value => AmmoRegen = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(OnBounceBulletDamage, "On Bounce Bullet Damage", menu, value => OnBounceBulletDamage = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(OnBounceBulletSpeed, "On Bounce Bullet Speed", menu, value => OnBounceBulletSpeed = value, fontSize: 50, forceUpper: false);
         }
         private static void AdvancedHealthStatsToggles(GameObject menu)
         {
             MenuHandler.CreateToggle(Revives, "Revives", menu, value => Revives = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Healing On Block adds to the amount of HP the player heals when they block", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(HealingOnBlock, "Healing On Block", menu, value => HealingOnBlock = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Health Decay increases the amount of time damage is applied to the player over", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(HealthDecay, "Health Decay", menu, value => HealthDecay = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Regenaration, "Regenaration", menu, value => Regenaration = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("The Size stat multiplies the current size of the player", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(Size, "Size", menu, value => Size = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Jumps, "Jumps", menu, value => Jumps = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(JumpHight, "Jump Hight", menu, value => JumpHight = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Gravity, "Gravity", menu, value => Gravity = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("What i use as the total recoil of bodyrecoil * chargerecoil * recoilmultiplier", menu, out TextMeshProUGUI _, 40);
             MenuHandler.CreateToggle(Recoil, "Recoil", menu, value => Recoil = value, fontSize: 50, forceUpper: false);
             MenuHandler.CreateToggle(BodyRecoil, "Body Recoil", menu, value => BodyRecoil = value, fontSize: 50, forceUpper: false);
@@ -593,32 +617,50 @@ namespace CatInfo
         {
             MenuHandler.CreateText("%Health Culling is how much of the target's max HP adds as additional damage", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(HealthCulling, "%Health Culling", menu, value => HealthCulling = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletRange, "Bullet Range", menu, value => BulletRange = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletSpeed, "Bullet Speed", menu, value => BulletSpeed = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Bullet Size makes ur bullet bigger", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(BulletSize, "Bullet Size", menu, value => BulletSize = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Projectile Size makes ur projectiles bigger", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(ProjectileSize, "Bullet Size", menu, value => ProjectileSize = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Projectile Size And Bullet Size are diffrent but idk in what way", menu, out TextMeshProUGUI _, 40);
             MenuHandler.CreateToggle(ProjectileSpeed, "Projectile Speed", menu, value => ProjectileSpeed = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Damage Growth adjusts the damage of the bullet after it moves 10 units", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(DamageGrowth, "Damage Growth", menu, value => DamageGrowth = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletSpread, "Bullet Spread", menu, value => BulletSpread = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(EvenBulletSpread, "Even Bullet Spread", menu, value => EvenBulletSpread = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateText("Bullet Drag is how much the bullet is affected by simulated air resistance", menu, out TextMeshProUGUI _, 60);
             MenuHandler.CreateToggle(BulletDrag, "Bullet Drag", menu, value => BulletDrag = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletSlow, "Bullet Slow", menu, value => BulletSlow = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(BulletGravity, "Bullet Gravity", menu, value => BulletGravity = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(Knockback, "Knockback", menu, value => Knockback = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(TimeBetweenBursts, "Time Between Bursts", menu, value => TimeBetweenBursts = value, fontSize: 50, forceUpper: false);
         }
         private static void ChargedStatsToggles(GameObject menu)
         {
             MenuHandler.CreateToggle(ChargedDamageMultiplier, "Plasma Charged Damage Multiplier", menu, value => ChargedDamageMultiplier = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(ChargedSpeed, "Plasma Charged Speed", menu, value => ChargedSpeed = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(ChargedSpread, "Plasma Charged Spread", menu, value => ChargedSpread = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(ChargedEvenSpread, "Plasma Charged Even Spread", menu, value => ChargedEvenSpread = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(ChargedRecoil, "Plasma Charged Recoil", menu, value => ChargedRecoil = value, fontSize: 50, forceUpper: false);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 30);
             MenuHandler.CreateToggle(ChargedNumberOfProjectiles, "Plasma Charged Number Of Bullets", menu, value => ChargedNumberOfProjectiles = value, fontSize: 50, forceUpper: false);
         }
     }
